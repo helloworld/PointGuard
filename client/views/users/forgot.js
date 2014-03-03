28bdf75fa81cf80584f599da9b4e5f83c85ebcc9
Sashank Thupukari <sthupukari@gmail.com>
2014-03-02 19:52:05 -0500

    New UI, No functionality
    Just the good looks

---
+++ client/views/users/forgot.js
@@ -0,0 +1,25 @@
+Template.forgot.events = {
+  'click input[type=submit]': function(e){
+    e.preventDefault();
+
+    var options = {
+      email: $('#email').val()
+    };
+
+    Accounts.forgotPassword(options, function(error){
+      if(error){
+        forgotflash(error.reason, "error");
+      }else{
+        Router.go('/signin');
+        forgotflash("Password reset link sent!");
+      }
+    });
+
+  }
+};
+
+Template.forgot.helpers({
+  messages: function () {
+    return forgotMessages.find();
+  },
+})
